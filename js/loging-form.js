6c4307004547f513b65f93ddaa30a885b7424c17
Aftab Farhan <aftabfarhan324@gmail.com>
2025-08-22 23:58:37 +0600

    alert addes amd home pages add

--- js/loging-form.js
+++ js/loging-form.js
@@ -1,6 +1,30 @@
 
 // Loging Button
 document.getElementById('loging-btn')
-  .addEventListener('click', function () {
-    console.log('Fing Button')
+.addEventListener('click', function (e) {
+    e.preventDefault();
+    // Declier Compear Varible
+    let mobileNumber = 12345678910;
+    let pin = 1234;
+
+    //Mobile Number
+    let valideMobileNumber = document.getElementById('mobile-number').value;
+    let mobileNumberConvart = parseInt(valideMobileNumber);
+    
+    // Pin Number Convart
+    let pinNumber = document.getElementById('pin-number').value;
+    
+    let pinValueConvart = parseInt(pinNumber);
+   
+    // If-Else-conditions Chack
+    if(mobileNumberConvart === mobileNumber 
+      && pinValueConvart === pin ){
+      window.location.href = "./homepage.html";
+    }else{
+      alert('Invalid Password');
+    }
+    
+
+    
+
   })
