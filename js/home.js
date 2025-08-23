f436e8b00ce9bdb3029099fb73e743153b178f93
Aftab Farhan <aftabfarhan324@gmail.com>
2025-08-23 21:26:34 +0600

    code add in js file

--- js/home.js
+++ js/home.js
@@ -4,16 +4,25 @@ document.getElementById('add-money-btn')
 
     const bankSectite = document.getElementById('bank-sectite').value;
  
-     const bankAccount = parseInt(document.getElementById('bank-account').value);
-
+     const bankAccount = document.getElementById('bank-account').value;
+    
      const addAmount = parseInt(document.getElementById('add-amount').value);
 
      const pinNumber = parseInt(document.getElementById('pin-number').value);
 
+
      const courrentBlance = parseInt(document.getElementById('current-blance').innerText);
      
      const totalCurrentBlance = addAmount + courrentBlance;
+     if(bankAccount.length < 11 ){
+      alert('Please Enter 11 Diget Bank Number');
+      return;
+     }
 
+     if(pinNumber !== 1234){
+      alert('Please Enter valid Pin Number');
+      return;
+     }
      document.getElementById('current-blance').innerText = totalCurrentBlance;
 
      
